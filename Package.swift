af0058f38fcf6472f7b8cfde1a964aaecc364b48
Mert YAMAN <mertyaman.dev@gmail.com>
2026-04-16 01:12:58 +0300

    chore: update binary to v1.0.2 - rename Cypien class, add API key support

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CypienSDK",
-            url: "https://github.com/Cypien-AI/cypien-ios-sdk/releases/download/v1.0.1/CypienSDK-1.0.1.xcframework.zip",
-            checksum: "1f7222d8120f8e9ef1629e9b91dee98359aa781e91eb7983dde410688900552a"
+            url: "https://github.com/Cypien-AI/cypien-ios-sdk/releases/download/v1.0.2/CypienSDK-1.0.2.xcframework.zip",
+            checksum: "109b26ef16e32810b2c94fb6b1cf2cff27acfd3cf005b437cc9b1f34db7affb4"
         )
     ]
 )
